af34db84e24e9067a68b9000e909c159099d8e75
George Konovalov <george.konovalov@gmail.com>
2025-08-09 12:37:30 +0300

    Add query bugs

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello there'))
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+})
 
 app.listen(3030, () => console.log('Server ready at port 3030'))
